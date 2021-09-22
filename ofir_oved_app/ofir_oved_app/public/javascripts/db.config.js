69814db08358c7370e096a952494225d0d4afc1d
OfirOved <ofirov@post.bgu.ac.il>
2021-09-22 15:00:34 +0300

    upload exe 6

---
+++ ofir_oved_app/ofir_oved_app/public/javascripts/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "xx18OA$$",
+    DB: "mysql"
+};
--- ofir_oved_app/server.js
+++ ofir_oved_app/server.js
@@ -10,25 +10,26 @@ app.use(bodyparser.urlencoded({extended: true}));
 
 //simple route
 app.get("/", (req, res) => {
-    res.json({message: "This is my basic route."});
+    res.json({message: "Welcome to web course example application."});
 });
 
-// Create a route for getting all contacts
-app.get("/contacts", function(req, res){
-    sql.query("SELECT * FROM contacts", (err, mysqlres) => {
+// Create a route for getting all customers
+app.get("/customers", function(req, res){
+    sql.query("SELECT * FROM customers", (err, mysqlres) => {
         if (err) {
         console.log("error: ", err);
-        res.status(400).send({message: "error in getting all contacts: " + err});
+        res.status(400).send({message: "error in getting all customers: " + err});
         return;
         }
-        console.log("got all contacts...");
+        console.log("got all customers...");
         res.send(mysqlres);
         return;
     });
 });
     
+
 //set port, listen for requests
-app.listen(8080, () => {
-    console.log("Server is running on port 8080.");
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.");
 });
 
